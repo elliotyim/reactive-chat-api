de78348761593cfaa8884dc15b7d5b864331e9cc
Elliot Yim <yimsb02@naver.com>
2021-07-17 19:10:35 +0900

    Initialize a project

---
+++ src/main/java/com/elldev/reactivechat/ReactivechatApplication.java
@@ -0,0 +1,13 @@
+package com.elldev.reactivechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elldev/reactivechat/ReactivechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elldev.reactivechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
