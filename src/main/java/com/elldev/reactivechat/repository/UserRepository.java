ebe101c5614959946faef16fc65c759cf479cbf6
Elliot Yim <yimsb02@naver.com>
2021-07-20 22:22:56 +0900

    Implement UserRepository

---
+++ src/main/java/com/elldev/reactivechat/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.elldev.reactivechat.repository;
+
+import com.elldev.reactivechat.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
