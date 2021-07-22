f976a6e82a49d81415e693220ab05e12355523ba
Elliot Yim <yimsb02@naver.com>
2021-07-22 21:05:22 +0900

    Add base error message & error code for exceptions

---
+++ src/main/java/com/elldev/reactivechat/constant/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.elldev.reactivechat.constant;
+
+public class ErrorCode {
+    public static final Integer INVALID_EMAIL_PROVIDED = 100;
+    public static final Integer INVALID_NAME_PROVIDED = 101;
+    public static final Integer PASSWORD_NOT_PROVIDED = 102;
+    public static final Integer USER_ALREADY_EXIST = 200;
+
+    public static final Integer UNDEFINED_ERROR = 9999;
+}
--- src/main/java/com/elldev/reactivechat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/elldev/reactivechat/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.elldev.reactivechat.exception;
 
-import com.elldev.reactivechat.exception.BadRequestException;
+import com.elldev.reactivechat.constant.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +14,25 @@ import java.util.Map;
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-    private int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    private final Integer BASE_STATUS = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    private final Integer BASE_ERROR_CODE = ErrorCode.UNDEFINED_ERROR;
 
     @ExceptionHandler(value = Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity handleExceptions(Exception e) {
+        int status = BASE_STATUS;
+
         Map<String, Object> payload = new HashMap<>();
+        payload.put("message", "Error occurred!");
+        payload.put("errorCode", BASE_ERROR_CODE);
 
         if (e instanceof BadRequestException) {
             status = HttpStatus.BAD_REQUEST.value();
             payload.put("message", e.getMessage());
             payload.put("errorCode", ((BadRequestException) e).errorCode);
-            log.error(e.getMessage(), e);
         }
+
+        log.error(e.getMessage(), e);
 
         return ResponseEntity.status(status).body(payload);
     }
