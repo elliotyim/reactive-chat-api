b89b598c3759f46f79e7039744ccee797a044cf4
Elliot Yim <yimsb02@naver.com>
2021-07-24 17:51:09 +0900

    Change the way of getting configuration properties in WebConfig

--- src/main/java/com/elldev/reactivechat/config/WebConfig.java
+++ src/main/java/com/elldev/reactivechat/config/WebConfig.java
@@ -1,19 +1,29 @@
 package com.elldev.reactivechat.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConfigurationProperties("web")
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${allowed-origins}")
     private String[] origins;
+    private boolean ssl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(origins);
     }
+
+    @Bean
+    public boolean isHttpSecure() {
+        return this.ssl;
+    }
+
+
 }
