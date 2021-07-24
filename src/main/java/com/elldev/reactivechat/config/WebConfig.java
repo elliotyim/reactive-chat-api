0ad2a7cd47bea4afa72871327cf9a2f606a5e75f
Elliot Yim <yimsb02@naver.com>
2021-07-24 20:08:48 +0900

    Remove redisson & apply @Value

--- src/main/java/com/elldev/reactivechat/config/WebConfig.java
+++ src/main/java/com/elldev/reactivechat/config/WebConfig.java
@@ -1,17 +1,18 @@
 package com.elldev.reactivechat.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ConfigurationProperties("web")
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${web.allowed-origins}")
     private String[] origins;
+
+    @Value("${web.ssl}")
     private boolean ssl;
 
     @Override
