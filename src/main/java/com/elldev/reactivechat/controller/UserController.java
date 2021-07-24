28af065fb188883480ff7472c88e34e69ca64fba
Elliot Yim <yimsb02@naver.com>
2021-07-24 17:59:55 +0900

    Implement signIn & signInCheck

--- src/main/java/com/elldev/reactivechat/controller/UserController.java
+++ src/main/java/com/elldev/reactivechat/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.elldev.reactivechat.controller;
 
 import com.elldev.reactivechat.dto.UserDto;
+import com.elldev.reactivechat.entity.UserSession;
+import com.elldev.reactivechat.exception.UserSessionNotFoundException;
 import com.elldev.reactivechat.service.UserService;
 import com.elldev.reactivechat.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @RestController
 @RequestMapping("users")
@@ -16,10 +21,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @GetMapping
+    public ResponseEntity<UserDto> checkIfSignedIn(
+            @CookieValue(value = "token") String token
+    ) throws Exception {
+        UserSession userSession = userService.getUserSessionByToken(token);
+        UserDto userDto = userService.getUserDtoByUserSession(userSession);
+        return ResponseEntity.ok(userDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<UserDto> signIn(
+            @RequestBody UserDto userDto,
+            HttpServletResponse response
+    ) throws Exception {
+        UserValidator.checkSignInUserInput(userDto);
+
+        UserDto signedInUser = userService.signIn(userDto);
+        UserSession userSession = userService.storeUserSession(userDto);
+        String cookie = userService.generateCookie("token", userSession.getToken());
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie);
+        return ResponseEntity.ok(signedInUser);
+    }
+
     @PostMapping
     public ResponseEntity<UserDto> signUp(@RequestBody UserDto userDto) throws Exception {
         UserValidator.checkRegistrationUserInput(userDto);
-        UserDto registeredUser = userService.register(userDto);
-        return ResponseEntity.ok(registeredUser);
+        UserDto registeredUser = userService.signUp(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED.value()).body(registeredUser);
     }
 }
